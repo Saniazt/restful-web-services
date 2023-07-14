74a5c6d627ce77a980fa2e9859a439360a605517
saniazt <saniazt@gmail.com>
2023-07-14 17:24:29 +0300

    versioning+filtering

--- src/main/java/com/saniazt/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/saniazt/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.saniazt.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +9,10 @@ import java.time.LocalDate;
 public class User {
     private int id;
     @Size(min = 2,message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
     @Past
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(int id, String name, LocalDate birthDate) {
--- src/main/java/com/saniazt/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/saniazt/rest/webservices/restfulwebservices/user/UserResource.java
@@ -2,6 +2,8 @@ package com.saniazt.rest.webservices.restfulwebservices.user;
 
 import jakarta.validation.Valid;
 import jakarta.validation.executable.ValidateOnExecution;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,10 +25,14 @@ public class UserResource {
 
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user==null) throw new UserNotFoundException("id"+id);
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder linkBuilder = WebMvcLinkBuilder
+                .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkBuilder.withRel("all-users"));
+        return entityModel;
     }
 
 
